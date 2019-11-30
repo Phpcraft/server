22f419641944d1fa67eefe5992a854a622f3c64b
Tim "timmyRS" Speckhals <github@timmyrs.de>
2019-11-30 21:01:40 +0100

    Update craft/core to 0.2.4

--- server.php
+++ server.php
@@ -7,6 +7,7 @@ if(empty($argv))
 require __DIR__."/vendor/autoload.php";
 use Phpcraft\
 {Command\Command, Event\ServerConsoleEvent, Event\ServerTickEvent, IntegratedServer, PluginManager};
+use pas\pas;
 $server = IntegratedServer::cliStart("Phpcraft Server", [
 	"groups" => [
 		"default" => [
@@ -33,19 +34,11 @@ echo "Loading plugins...\n";
 PluginManager::loadPlugins();
 echo "Loaded ".count(PluginManager::$loaded_plugins)." plugin(s).\n";
 $server->ui->render();
-$next_tick = microtime(true) + 0.05;
-do
+pas::on("stdin_line", function(string $msg) use (&$server)
 {
-	$start = microtime(true);
-	$server->accept();
-	$server->handle();
-	while($msg = $server->ui->render(true))
+	if(!Command::handleMessage($server, $msg) && !PluginManager::fire(new ServerConsoleEvent($server, $msg)))
 	{
-		if(Command::handleMessage($server, $msg) || PluginManager::fire(new ServerConsoleEvent($server, $msg)))
-		{
-			continue;
-		}
-		$msg = [
+		$server->broadcast([
 			"translate" => "chat.type.announcement",
 			"with" => [
 				[
@@ -55,19 +48,13 @@ do
 					"text" => $msg
 				]
 			]
-		];
-		$server->broadcast($msg);
+		]);
 	}
-	if($next_tick < microtime(true))
-	{
-		$next_tick += 0.05;
-		PluginManager::fire(new ServerTickEvent($server, $next_tick < microtime(true)));
-	}
-	if(($remaining = (0.001 - (microtime(true) - $start))) > 0)
-	{
-		time_nanosleep(0, $remaining * 1000000000);
-	}
-}
-while($server->isOpen());
+});
+$server->open_condition->add(function(bool $lagging) use (&$server)
+{
+	PluginManager::fire(new ServerTickEvent($server, $lagging));
+}, 0.05);
+pas::loop();
 $server->ui->add("Server is not listening on any ports and has no clients, so it's shutting down.");
 $server->ui->render();
